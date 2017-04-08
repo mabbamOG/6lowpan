dd649cb1e6df0552cf4f83a9fbc8ec4a17e92bf2
@mabbamOG <mariobarbara@protonmail.ch>
2017-04-08 20:11:04 +0200

    change to stop script

--- stop_6lo.fish
+++ stop_6lo.fish
@@ -2,7 +2,7 @@
 test (id -u) -ne 0; and echo 'ERROR: please run script as superuser!'; and exit
 
 # disable and delete 6lowpan interface
-sudo ip link set wpan0 down
 sudo ip link set lowpan0 down
+sudo ip link set wpan0 down
 sudo ip link delete dev lowpan0
 echo 'DONE!'
